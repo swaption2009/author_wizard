f6322936ea520a60c2498ebad91e887ce3f1c6b1
Jason Swett <jason@benfranklinlabs.com>
2016-07-07 11:09:27 -0700

    Create Book resource.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module AuthorWizard
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
