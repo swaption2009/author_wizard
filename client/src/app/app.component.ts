f87b1c7f03a5d0435f5975ef8c09df2cd24fbde6
Jason Swett <jason@benfranklinlabs.com>
2016-07-07 20:41:59 -0700

    Get BookListComponent hooked up.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BookListComponent } from './book-list';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [BookListComponent]
 })
 export class AppComponent {
   title = 'app works!';
