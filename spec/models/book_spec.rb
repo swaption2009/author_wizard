04e7e173a88f54d9464b34f4c5c4a2d039dfe63c
Jason Swett <jason@benfranklinlabs.com>
2016-07-07 17:38:50 -0700

    Add spec for Book.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryGirl.create(:book)
+  end
+
+  describe 'name' do
+    it 'should be present' do
+      @book.name = nil
+
+      expect(@book).not_to be_valid
+    end
+  end
 end
