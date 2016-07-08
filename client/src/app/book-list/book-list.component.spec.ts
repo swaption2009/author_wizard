aea3b564a67ec4f9a8c49dac8867318907d9d1cd
Jason Swett <jason@benfranklinlabs.com>
2016-07-07 20:09:51 -0700

    Add book-list component.

---
+++ client/src/app/book-list/book-list.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+
+import { BookListComponent } from './book-list.component';
+
+describe('Component: BookList', () => {
+  it('should create an instance', () => {
+    let component = new BookListComponent();
+    expect(component).toBeTruthy();
+  });
+});
