e45ca09212998140ae4dfdc10870298632a6e8ff
Jason Swett <jason@benfranklinlabs.com>
2016-07-22 09:11:14 -0400

    Add NewBookComponent.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { BookListComponent } from './book-list';
+import { NewBookComponent } from './new-book';
 
 const routes: RouterConfig = [
-  { path: '', component: BookListComponent }
+  { path: '', component: BookListComponent },
+  { path: 'new-book', component: NewBookComponent }
 ];
 
 export const appRouterProviders = [
---
+++ client/src/app/new-book/new-book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-new-book',
+  templateUrl: 'new-book.component.html',
+  styleUrls: ['new-book.component.css']
+})
+export class NewBookComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
