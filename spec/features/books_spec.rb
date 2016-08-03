ecfb1ea507842453a9b59c4274fc72bb1e1feccf
Jason Swett <jason@benfranklinlabs.com>
2016-08-03 08:31:48 -0400

    Add more integration tests.

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -3,7 +3,32 @@ require 'rails_helper'
 feature 'Books', js: true do
   scenario 'list page' do
     visit '/'
-
     expect(page).to have_content('Books')
+  end
+
+  scenario 'new book' do
+    visit '/'
+    click_on 'New Book'
+    fill_in 'Name', with: 'A Little History of the World'
+    click_on 'Submit'
+    expect(page).to have_content('A Little History of the World')
+  end
+
+  scenario 'edit book' do
+    create(:book, name: 'The Lost Art of Finding Our asdf')
+
+    visit '/'
+    click_on 'Edit'
+    fill_in 'Name', with: 'The Lost Art of Finding Our Way'
+    click_on 'Submit'
+    expect(page).to have_content('The Lost Art of Finding Our Way')
+  end
+
+  scenario 'delete book' do
+    create(:book, name: 'Eat to Live')
+
+    visit '/'
+    click_on 'Delete'
+    expect(page).not_to have_content('Eat to Live')
   end
 end
